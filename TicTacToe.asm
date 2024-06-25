78ba613e2a531a78dfb86a01a9bf48d3975c6aa4
JOSE PABLO BRENES COTO <josepablo.brenes@ucr.ac.cr>
2024-06-25 10:56:59 -0600

    second version

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -1,6 +1,6 @@
 .data
-display: .space 1024
-table: .space 36
+display: .space 1024 #Para el bitmap
+table: .space 36 #Para las respuestas de las casillas
 user: .asciiz "Jugador " #No sirvio
 next: .asciiz "\n"
 num: .asciiz "Ingresar nÃºmero de jugada (0-8): "
@@ -12,13 +12,16 @@ tiePlay: .asciiz "Empate \n"
 .text
 .globl main
 
+#Es el punto de entrada del programa. Este inicializa el juego llamando a initTable y llama las funciones correspondientes para 
+#crear el tablero. Además, junto con Game realiza la lógica principal del juego, donde pide la entrada al usuario, coloca la ficha 
+#y revisa el ganador.
 main:
     li $t0,0
     li $t1,0
-    li $t5, 4
-    li $t9,0
+    li $t5, 4   #Calcula posiciones del tablero
+    li $t9,0 # Contador de jugadas
     jal initTable
-    li $t0, 0xFFFFFF
+    li $t0, 0xFFFFFF #Blanco 
     li $t1, 20
     jal createVerticalLine
     li $t1, 320
@@ -30,5 +33,17 @@ main:
     li $t2, 0
     jal createHorizontalLine
 
-    li $t0, 0x00FF00
+    li $t0, 0x00FF00  #Para el primero jugador(verde)
     li $t4, 1
+#realiza la lógica principal del juego, donde pide la entrada al usuario, coloca la ficha, cambia de usuario y revisa el ganador.
+game:
+    li $t3, 20  #calcula los desplazamientos en el tablero
+    jal userInput
+    jal playerMovement
+    jal selected
+    jal drawMovement
+    jal winner
+    jal changePlayer
+    
+    j game
+
